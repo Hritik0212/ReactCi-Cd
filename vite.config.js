4f47ea682b4ed68e59fafa356568fcce7c36a624
Hritik Bhandari <hritikbhandari02@gmail.com>
2024-10-29 04:48:43 +0530

    added codes

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { configdefaults } from 'vitest/config'
+import { configDefaults } from 'vitest/config'
 
 // https://vite.dev/config/
 
 
 
-export default define config({
+export default defineConfig({
   plugins: [react()],
   tests: {
     globals: true,
     environment: 'jsdom',
     setupfiles: './src/setupTests.js',
-    exclude: [... configdefaults.exclude, 'e2e/*'],
+    exclude: [... configDefaults.exclude, 'e2e/*'],
   },
 })
